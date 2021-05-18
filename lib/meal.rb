c00c1152d3917713d5725ec8410fedb7665e431e
Shasha Yan <shashayan@gmail.com>
2021-05-18 03:48:24 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,16 @@
 class Meal
+  attr_reader :waiter, :customer, :total, :tip
+
+  def initialize(waiter, customer, total, tip=0)
+    @waiter = waiter
+    @customer = customer
+    @total = amount
+    @tip = 0
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
 end
