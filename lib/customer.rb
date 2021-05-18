d1c4c365eb32a8cb80182e9000b76c3d8eda5abb
Shasha Yan <shashayan@gmail.com>
2021-05-18 03:30:26 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,16 @@
 class Customer
+  attr_reader :name, :age
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+
+  def self.all
+    @@all
+  end 
+
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
   
 end
